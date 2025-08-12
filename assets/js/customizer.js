9bdd101c1d81d16044d3d0b04eaddd8f7815bf54
emmanueljg33 <41129446+emmanueljg33@users.noreply.github.com>
2025-08-12 02:42:54 -0500

    Enlarge mockup preview

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -48,7 +48,7 @@
     loadFont(font);
     $preview.text(display).css({
       fontFamily: font,
-      fontSize: (inches/3)+'px',
+      fontSize: (inches/2)+'px',
       color:'#fff',
       textShadow: glow(color)
     });
